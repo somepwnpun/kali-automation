76a7df07fce10ec2a88dce3f8de3eb86e8feeda2
mseilmaier <seilmaierm@gmail.com>
2024-09-07 23:31:41 +0200

    bugfixes

--- local/provision.sh
+++ local/provision.sh
@@ -3,20 +3,27 @@
 # Update the package list
 echo "Updating package list..."
 sudo apt update -y
+sudo apt install software-properties-common -y
 
 # Install Ansible
 echo "Installing Ansible..."
 sudo apt install ansible -y
 
+echo "Installing VBox Guest Addition Dependencies..."
+sudo apt install dkms build-essential linux-headers-$(uname -r)
+
+echo "Installing Ansible Community Plugins..."
+ansible-galaxy install gantsign.visual-studio-code
+
 # Run the Ansible playbook
-PLAYBOOK_PATH="kali-automation/local/build.yml"
+PLAYBOOK_PATH="build.yml"
 
 if [ -f "$PLAYBOOK_PATH" ]; then
   echo "Running Ansible playbook..."
-  ansible-playbook "$PLAYBOOK_PATH"
+  ansible-playbook "$PLAYBOOK_PATH" -e "username=$USER"
 else
   echo "Playbook not found at $PLAYBOOK_PATH. Exiting."
   exit 1
 fi
 
-echo "Ansible playbook executed successfully!"
+echo "Ansible playbook run ended!"
